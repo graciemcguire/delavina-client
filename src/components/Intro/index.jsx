3952f65e77e95c3beea459237782e1476304ac82
Gracie McGuire <m.graciemcguire@gmail.com>
2025-02-19 16:18:07 -0500

    initial commit

---
+++ src/components/Intro/index.jsx
@@ -0,0 +1,58 @@
+import styles from "./style.module.css";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
+
+export default function index() {
+  const backgroundImage = useRef(null);
+  const introImage = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top",
+        end: "+=500px",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    timeline
+      .from(backgroundImage.current, { clipPath: "inset(15%)" })
+      .to(introImage.current, { height: "200px" }, 0);
+  }, []);
+
+  return (
+    <div className={styles.intro}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
+        <Image
+          src={"/images/maxresdefault.jpg"}
+          fill={true}
+          alt="background image"
+        />
+      </div>
+
+      <div className={styles.introContainer}>
+        <div
+          ref={introImage}
+          data-scroll
+          data-scroll-speed=".3"
+          className={styles.introImage}
+        >
+          <Image
+            src={"/images/intro-image.jpg"}
+            fill={true}
+            alt="intro image"
+          />
+        </div>
+        <h1 data-scroll data-scroll-speed=".7">
+          Gracie & Antonia
+        </h1>
+      </div>
+    </div>
+  );
+}
